e29f4d7a8b8d94a9547604f5b6fa47f00f25df8f
Thiago <thiagolvaz@gmail.com>
2020-08-04 18:25:31 -0300

    Scripts para criação das tabelas do banco e carga inicial dos dados

---
+++ back-end/hotmart-challenge-api/src/main/resources/db/migration/V02__inserir_categorias.sql
@@ -0,0 +1,6 @@
+insert into categoria(id, nome) values(1,'business');
+insert into categoria(id, nome) values(2,'entertainment');
+insert into categoria(id, nome) values(3,'health');
+insert into categoria(id, nome) values(4,'science');
+insert into categoria(id, nome) values(5,'sports');
+insert into categoria(id, nome) values(6,'technology');
